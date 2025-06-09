003f12539f655a66a4712c0e45ee4621026dab47
Eduardo Douglas <deixeseurecado@gmail.com>
2025-06-09 15:03:06 +0000

    altera novamente

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-const geminiApiKey = process.env.REACT_APP_GEMINI_API_KEY; // Mantenha assim por enquanto para depurar
+const geminiApiKey = process.env.GEMINI_API_KEY; // Mantenha assim por enquanto para depurar
 
 console.log('Valor da GEMINI_API_KEY:', geminiApiKey); // Adicione esta linha para depurar
 
