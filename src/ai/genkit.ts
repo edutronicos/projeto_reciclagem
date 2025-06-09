bc6a4530e80c29abd0c5d8387ea0dce505f8aa38
Eduardo Douglas <deixeseurecado@gmail.com>
2025-06-09 12:56:30 +0000

    altera o nome para consumo da api.

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
+const geminiApiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+
+if (!geminiApiKey) {
+  console.error('ERRO: GEMINI_API_KEY não está definida nas variáveis de ambiente!');
+};
+
 export const ai = genkit({
-  plugins: [googleAI({ apiKey: process.env.GEMINI_API_KEY })],
+  plugins: [googleAI({ apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY })],
   model: 'googleai/gemini-2.0-flash',
 });
